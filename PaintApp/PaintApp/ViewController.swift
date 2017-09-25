4bcdc5570115742ab21b2126d8fa68b357d34774
Sara <saraahmad1114@yahoo.com>
2017-09-25 04:36:12 -0400

    configuring strokes

--- PaintApp/PaintApp/ViewController.swift
+++ PaintApp/PaintApp/ViewController.swift
@@ -10,9 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var drawingPad: UIImageView!
+    
+    var lastPoint = CGPoint.zero
+    
+    var userTouched = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first{
+            self.lastPoint = touch.location(in: self.view)
+        }
+    }
+    
+    func drawOnDrawingPad (originalPoint: CGPoint, toPoint: CGPoint) {
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        self.drawingPad.image?.draw(in: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
+        var context = UIGraphicsGetCurrentContext()
+        
+        context?.move(to: CGPoint(x: originalPoint.x, y: originalPoint.y))
+        context?.addLine(to: CGPoint(x: toPoint.x, y: toPoint.y))
+        
+        context?.setBlendMode(CGBlendMode.normal)
+        context?.setLineCap(CGLineCap.round)
+        context?.setLineWidth(8)
+        //context?.setStrokeColor(UIColor(red: 0, green: 0, blue: 0, alpha: 1.0))
+        var colorNeeded = UIColor.black
+        
+        context?.setStroke(UIColor.black)
+        
+        context?.strokePath()
+        
+        
+        self.drawingPad.image = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        <#code#>
     }
 
     override func didReceiveMemoryWarning() {
